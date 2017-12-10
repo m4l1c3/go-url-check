6877c1e06de313caf6535c3841297388781c6f04
m4l1c3 <jon@jonsomnia.com>
2017-12-09 17:59:49 -0700

    Adding file output writing

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 
 	color "github.com/fatih/color"
 	requests "github.com/hiroakis/go-requests"
@@ -20,6 +22,47 @@ type StringSet struct {
 	set map[string]bool
 }
 
+//URLResponse struct for housing an HTTPResponse
+type URLResponse struct {
+	StatusCode int
+	URL        string
+}
+
+//URLResponseSet struct for housing a map of Responses
+type URLResponseSet struct {
+	set map[URLResponse]bool
+}
+
+//Add to ResponseSet
+func (set *URLResponseSet) Add(r URLResponse) bool {
+	_, found := set.set[r]
+	set.set[r] = true
+	return !found
+}
+
+//AddRange add a list of elements to a set
+func (set *URLResponseSet) AddRange(rr []URLResponse) {
+	for _, r := range rr {
+		set.set[r] = true
+	}
+}
+
+//Contains Test if an element is in a set
+func (set *URLResponseSet) Contains(r URLResponse) bool {
+	_, found := set.set[r]
+	return found
+}
+
+//ContainsAny Check if any of the elements exist
+func (set *URLResponseSet) ContainsAny(rr []URLResponse) bool {
+	for _, r := range rr {
+		if set.set[r] {
+			return true
+		}
+	}
+	return false
+}
+
 //State struct for running state
 type State struct {
 	ProxyURL       string
@@ -29,6 +72,7 @@ type State struct {
 	Wordlist       StringSet
 	StatusCodes    IntSet
 	WriteOutput    bool
+	Responses      URLResponseSet
 }
 
 //Add to StringSet
@@ -38,7 +82,7 @@ func (set *StringSet) Add(s string) bool {
 	return !found
 }
 
-//AddRand add a list of elements to a set
+//AddRange add a list of elements to a set
 func (set *StringSet) AddRange(ss []string) {
 	for _, s := range ss {
 		set.set[s] = true
@@ -82,9 +126,19 @@ func WriteOutput(state *State) (bool, error) {
 			color.Red("[!] Unable to write to %s, falling back to stdout.\n", state.OutputFileName)
 			return false, err
 		}
-		if outputFile != nil {
-			return true, nil
+		defer outputFile.Close()
+
+		for u := range state.Responses.set {
+			write, err := outputFile.WriteString(strconv.Itoa(u.StatusCode) + " " + u.URL + "\n")
+			if err != nil {
+				color.Red("Error writing file %s\n", err)
+			}
+			if write != nil {
+				continue
+			}
 		}
+		outputFile.Sync()
+		return true, nil
 	}
 	return false, nil
 }
@@ -134,6 +188,7 @@ func ParseArgs() *State {
 	s := State{
 		StatusCodes: IntSet{set: map[int]bool{}},
 		Wordlist:    StringSet{set: map[string]bool{}},
+		Responses:   URLResponseSet{set: map[URLResponse]bool{}},
 	}
 
 	flag.IntVar(&s.Threads, "t", 10, "Number of concurrent threads")
@@ -189,45 +244,68 @@ func Banner(state *State) {
 	}
 }
 
-//checkStatusCode evaluates response status codes and prints in a diff color
-func checkStatusCode(url string, statusCode int) {
+//PrintResponse evaluates response status codes and prints in a diff color
+func PrintResponse(url string, statusCode int) {
 	switch {
 	case statusCode > 499:
 		color.Red("[!] %s %d\n", url, statusCode)
 		break
 	case statusCode > 399:
-		color.Magenta("[!] %s %d\n", url, statusCode)
+		color.Magenta("[+] %s %d\n", url, statusCode)
 		break
 	case statusCode > 299:
-		color.Yellow("[!] %s %d\n", url, statusCode)
+		color.Yellow("[+] %s %d\n", url, statusCode)
 		break
 	default:
-		color.Green("[!] %s %d\n", url, statusCode)
+		color.Green("[+] %s %d\n", url, statusCode)
 		break
 	}
 }
 
-//checkURL does a GET for a URL
-func checkURL(url string) (int, error) {
+//PrefixURL with http if missing
+func PrefixURL(url string) string {
+	if !strings.HasPrefix(url, "http") {
+		url = "http://" + url
+	}
+	return url
+}
+
+//Request issue web request for given URL
+func Request(url string) requests.Response {
 	resp, err := requests.Get(url, nil, nil)
 
 	if err != nil {
-		return 0, err
+		color.Red("Error checking URL: %s\n", err)
 	}
 
-	checkStatusCode(url, resp.StatusCode())
-	return resp.StatusCode(), nil
+	return resp
+}
+
+//CheckURL does a GET for a URL
+func CheckURL(url string) (URLResponse, error) {
+	PrefixURL(url)
+	resp := Request(url)
+	PrintResponse(url, resp.StatusCode())
+	r := URLResponse{
+		StatusCode: resp.StatusCode(),
+		URL:        url,
+	}
+	return r, nil
 }
 
 //Process runtime config and execute
 func Process(state *State) {
 	for word := range state.Wordlist.set {
-		responseStatus, error := checkURL(word)
+		response, error := CheckURL(word)
 
 		if error != nil {
 			fmt.Println("Error getting URL ", error)
 		}
-		state.StatusCodes.Add(responseStatus)
+		state.Responses.Add(response)
+	}
+
+	if state.WriteOutput {
+		WriteOutput(state)
 	}
 }
 
